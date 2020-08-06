918050ecafade1ce341dea7a53590afacf6c8075
unknown <simonh72498@gmail.com>
2020-08-06 16:36:51 -0400

    completed mvp and stretch

--- src/item.py
+++ src/item.py
@@ -6,3 +6,16 @@ class Item:
         print(f"You have taken {self.name}")
     def on_drop(self):
         print(f"You have dropped {self.name}")
+    def __str__(self):
+        return f"\n\tName: {self.name}\n\tDescription: {self.description}"
+    def __repr__(self):
+        return self.__str__()
+
+class Weapon(Item):
+    def __init__(self, name, description, att):
+        self.att = att
+        super().__init__(name, description)
+    def __str__(self):
+        return f"\n\tName: {self.name}\n\tDescription: {self.description}\n\tAttack: {self.att}"
+    def __repr__(self):
+        return self.__str__()
--- src/room.py
+++ src/room.py
@@ -2,7 +2,10 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, item = []):
+    def __init__(self, name, description, item = [], monster = None):
         self.name = name
         self.description = description
+        self.monster = monster
         self.item = item;
+    def __str__(self):
+        return f"Room Name: {self.name}\nRoom Description: {self.description}\nItems in Room: {self.item}\nMonster in Room: {self.monster}"
